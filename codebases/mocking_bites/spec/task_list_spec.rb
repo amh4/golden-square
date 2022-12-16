d4c6c4ec65b56efc548ab0ce9a5331b23ee53166
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-16 11:33:59 +0000

    Unit mock tests completed for task_list.rb

--- codebases/mocking_bites/spec/task_list_spec.rb
+++ codebases/mocking_bites/spec/task_list_spec.rb
@@ -12,4 +12,37 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+
+  context "testing 'all' method" do
+    it "returns all tasks" do
+      task_list = TaskList.new
+      task_1 = double :task
+      task_2 = double :task
+      task_list.add(task_1)
+      task_list.add(task_2)
+      expect(task_list.all).to eq [task_1, task_2]
+    end
+  end
+
+  it "returns false because all tasks are not done" do
+    task_list = TaskList.new
+    task_1 = double :task
+    task_2 = double :task
+    expect(task_1).to receive(:complete?).and_return(true)
+    expect(task_2).to receive(:complete?).and_return(false)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq false
+  end
+
+  it "returns true because all tasks are done" do
+    task_list = TaskList.new
+    task_1 = double :task
+    task_2 = double :task
+    expect(task_1).to receive(:complete?).and_return(true)
+    expect(task_2).to receive(:complete?).and_return(true)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq true
+  end
 end
